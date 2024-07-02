855db60d30ef19e94dbaf0ac95d34b114251ad25
Pavel Tiguntsev <ptiguntsev@gmail.com>
2024-07-02 03:25:54 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <GetAndPost />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import GetAndPost from "./components/GetAndPost.vue";
+  //
+</script>
---
+++ src/components/GetAndPost.vue
@@ -0,0 +1,166 @@
+<template>
+  <v-container class="">
+    <v-responsive class="mx-auto" max-width="900">
+      <v-row>
+
+        <v-col cols="12" xs="6" sm="6">
+
+          <v-text-field label="IdInstance" placeholder="XXX" readonly v-model="idInstance" type="text"></v-text-field>
+
+
+          <v-text-field label="ApiToken Instance" placeholder="XXX" readonly v-model="apiTokenInstance"
+            type="text"></v-text-field>
+
+
+          <v-btn color="#5865f2" block class="ma-1" @click="initGetMethod('setSettings')">get Settings
+          </v-btn>
+
+          <v-btn color="#5865f2" block class="ma-1" @click="initGetMethod('getStateInstance')">get State Instance
+          </v-btn>
+
+
+          <v-container>
+            <v-card> <v-text-field label="Chat ID" placeholder="Chat ID" v-model="message.chatId"
+                type="text"></v-text-field>
+
+
+              <v-textarea block label="Message" model-value="" v-model="message.message" name="input-7-2"
+                variant="filled" auto-grow></v-textarea>
+
+              <div class="text-center">
+                <v-btn color="#5865f2" class="ma-1 center" @click="initPostMethod('sendMessage', this.message)">Send
+                  Message
+                </v-btn>
+              </div>
+            </v-card>
+          </v-container>
+
+
+
+          <v-container>
+            <v-card> <v-text-field label="Chat ID" placeholder="Chat ID" v-model="fileLink.chatId"
+                type="text"></v-text-field>
+              <v-text-field label="File Name" placeholder="File Name" v-model="fileLink.fileName"
+                type="text"></v-text-field>
+              <v-text-field label="Link" placeholder="Link" v-model="fileLink.urlFile" type="text"></v-text-field>
+              <div class="text-center">
+                <v-btn color="#5865f2" class="ma-1 center" @click="initPostMethod('sendFileByUrl', this.fileLink)">Send
+                  File
+                </v-btn>
+              </div>
+            </v-card>
+          </v-container>
+
+        </v-col>
+
+        <v-col cols="12" xs="6" sm="6">
+          <div class="is-error-msg" v-if="isError">
+            Error
+          </div>
+          <v-container fluid>
+            <v-textarea label="Response" class="response-area" :class="{ error: isError }" readonly v-model="response"
+              name="input-7-1" variant="filled" auto-grow></v-textarea>
+          </v-container>
+        </v-col>
+
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+
+<script lang="js">
+
+export default {
+  data() {
+    return {
+      idInstance: "1103953379",
+      apiTokenInstance: "5ca35bd9dda44eaebcafbae9bde5e56e156d890211af43ff83",
+      apiUrl: "https://1103.api.green-api.com",
+      mediaUrl: "https://1103.media.green-api.com",
+      message: {
+        chatId: "79668773633@c.us",
+        message: ""
+      },
+      fileLink: {
+        chatId: "79668773633@c.us",
+        urlFile: "",
+        fileName: ""
+      },
+      response: "",
+      isError: false
+    }
+  },
+  computed: {
+    ins_url() {
+      return `${this.apiUrl}/waInstance${this.idInstance}`
+    },
+  },
+  methods: {
+    initGetMethod(methodName) {
+      const vue = this;
+      vue.isError = false;
+      try {
+        fetch(this.getLinkByMethod(methodName))
+          .then(resp => {
+            console.log(resp);
+            if (!resp.ok) {
+              this.responseError(resp.statusText)
+              throw new Error(resp.statusText);
+            }
+            return resp.text()
+          })
+          .then(
+            response_context => {
+              vue.response = response_context;
+            }
+          )
+      } catch (e) {
+        vue.responseError(e)
+      }
+
+    },
+    initPostMethod(methodName, body) {
+      const vue = this;
+      vue.isError = false;
+      try {
+        fetch(this.getLinkByMethod(methodName), {
+          method: "POST",
+          body: JSON.stringify(body),
+        }).then(resp => {
+          if (!resp.ok) {
+            this.responseError(resp.statusText);
+            console.log({ resp });
+            throw new Error(resp.statusText);
+          }
+          return resp.text()
+        }).then(
+          response_context => {
+            vue.response = response_context;
+          }
+        )
+      } catch (e) {
+        vue.responseError(e)
+      }
+    },
+    getLinkByMethod(method) {
+      return `${this.ins_url}/${method}/${this.apiTokenInstance}`
+    },
+    responseError(errMsg) {
+      this.response = errMsg
+      this.isError = true;
+    }
+  }
+}
+
+</script>
+
+<style>
+.response-area.error {
+  border: 1px solid #ff5b5b;
+}
+
+.is-error-msg {
+  color: #ff5b5b;
+}
+</style>
